c7075409d00782875ba16e0708a87e1fd8c6b5fe
Mario Geiger <geiger.mario@gmail.com>
2021-07-19 18:45:33 +0200

    fix imap

--- jarzynski/_util.py
+++ jarzynski/_util.py
@@ -2,8 +2,8 @@ import jax
 import jax.numpy as jnp
 
 
-def narrow(x, dim, index):
-    return x[(Ellipsis,) * dim + (index,) + (Ellipsis,) * (len(x.shape) - dim - 1)]
+def narrow(dim, index):
+    return (slice(None, None, None),) * dim + (index,)
 
 
 def imap(f, in_axes, out_axes=0):
@@ -12,13 +12,15 @@ def imap(f, in_axes, out_axes=0):
         n = ns[0]
         assert len(set(ns)) == 1
 
-        b = [a if i is None else narrow(a, i, 0) for i, a in zip(in_axes, args)]
+        b = [a if i is None else a[narrow(i, 0)] for i, a in zip(in_axes, args)]
         outs = jax.tree_map(lambda x: jnp.empty(x.shape[:out_axes] + (n,) + x.shape[out_axes:]), f(*b))
 
-        def body(j, outs):
-            b = [a if i is None else narrow(a, i, j) for i, a in zip(in_axes, args)]
-            out = f(*b)
-            return jax.tree_multimap(lambda x, y: x.at[(Ellipsis,) * out_axes + (j,) + (Ellipsis,) * (len(x.shape)-out_axes-1)].set(out), outs, out)
+        def body(i, outs):
+            b = [a if in_axis is None else a[narrow(in_axis, i)] for in_axis, a in zip(in_axes, args)]
+            return jax.tree_multimap(
+                lambda out, fi: out.at[narrow(out_axes, i)].set(fi),
+                outs, f(*b)
+            )
 
         return jax.lax.fori_loop(0, n, body, outs)
 
